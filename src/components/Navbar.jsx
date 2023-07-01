faf5252e9c7713590ed75774072d3980d23365a5
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-01 20:51:39 +0530

    Add Topbar and Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaFacebookF, FaTwitter, FaGooglePlusG, FaInstagram, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [nav, setNav] = useState(false)
 
-export default Navbar
+  const handleNav = () => {
+    setNav((prev) => !prev)
+  };
+  
+  return (
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+      <ul className='hidden sm:flex px-4'>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='#gallery'>Gallery</a>
+        </li>
+        <li>
+          <a href='#deals'>Deals</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+      <div className='flex justify-between'>
+        <FaFacebookF className='mx-4' />
+        <FaGooglePlusG className='mx-4' />
+        <FaTwitter className='mx-4' />
+        <FaInstagram className='mx-4' />
+      </div>
+      <div onClick={handleNav} className='sm:hidden z-10'>
+        <FaBars size={20} className='mr-4 cursor-pointer' />
+      </div>
+      <div 
+        onClick={handleNav}
+        className={
+          nav
+            ? 'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 bg-black/90 left-0 top-0 w-full h-screen px-4 py-7 flex flex-col'
+            : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'
+        }
+      >
+        <ul className='w-full h-full text-center pt-12'>
+          <li className='text-2xl py-8'>
+            <a href='/'>Home</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href='#gallery'>Gallery</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href='#deals'>Deals</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineClockCircle, AiOutlinePhone} from 'react-icons/ai';
+import { wanderlustLogo } from '../assets';
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <div className='flex justify-between items-center px-4 py-2'>
+      <img src={wanderlustLogo} alt='wanderlust-logo' className='w-20 h-10 sm:w-40 sm:h-10' />
+      <div className='flex'>
+        <div className='hidden md:flex items-center px-6'>
+          <AiOutlineClockCircle size={20} className='mr-2 text-[var(--primary-dark)]' />
+          <p className='text-sm text-gray-700'>9:00 AM - 6:00 PM</p>
+        </div>
+        <div className='hidden md:flex items-center px-6'>
+          <AiOutlinePhone size={20} className='mr-2 text-[var(--primary-dark)]' />
+          <p className='text-sm text-gray-700'>1-555-444-1234</p>
+        </div>
+        <button>Quick book</button>
+      </div>
+    </div>
+  );
+};
 
-export default Topbar
+export default Topbar;
