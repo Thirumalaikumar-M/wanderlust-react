7ea347787e961f891c56260cb08629d6a1d2c13c
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-01 13:02:42 +0530

    Clean boilerplate and establish project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Topbar,
+  Navbar,
+  Hero,
+  Activities,
+  Booking,
+  Destinations,
+  Gallery,
+  Feedbacks,
+  Contact,
+  Footer,
+} from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Topbar />
+      <Navbar />
+      <Hero />
+      <Activities />
+      <Booking />
+      <Destinations />
+      <Gallery />
+      <Feedbacks />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Topbar from "./Topbar";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Activities from "./Activities";
+import Booking from "./Booking";
+import Destinations from "./Destinations";
+import Gallery from "./Gallery";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+export {
+  Topbar,
+  Navbar,
+  Hero,
+  Activities,
+  Booking,
+  Destinations,
+  Gallery,
+  Feedbacks,
+  Contact,
+  Footer,
+};
