f95ac6a53fd272de71a194309a6206f4039f9b09
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-01 13:37:06 +0530

    Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,49 @@
+import adventure from "./adventure.jpg";
+import burjkhalifa from "./burjkhalifa.jpg";
+import contactImg from "./contact-img.jpg";
+import culture from "./culture.jpg";
+import foods from "./foods.jpg";
+import gallery1 from "./gallery1.jpg";
+import gallery2 from "./gallery2.jpg";
+import gallery3 from "./gallery3.jpg";
+import gallery4 from "./gallery4.jpg";
+import gallery5 from "./gallery5.jpg";
+import heroBg from "./hero-bg.avif";
+import libertystatue from "./libertystatue.jpg";
+import machupicchu from "./machupicchu.jpg";
+import mysorepalace from "./mysorepalace.jpg";
+import paris from "./paris.jpg";
+import person1 from "./person1.jpg";
+import person2 from "./person2.jpg";
+import person3 from "./person3.jpg";
+import tajmahal from "./tajmahal.jpg";
+import themepark from "./themepark.jpg";
+import viewpoint from "./viewpoint.jpg";
+import wanderlustLogo from "./wanderlust-logo.webp";
+import wildlife from "./wildlife.jpg";
+
+export {
+  adventure,
+  burjkhalifa,
+  contactImg,
+  culture,
+  foods,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  heroBg,
+  libertystatue,
+  machupicchu,
+  mysorepalace,
+  paris,
+  person1,
+  person2,
+  person3,
+  tajmahal,
+  themepark,
+  viewpoint,
+  wanderlustLogo,
+  wildlife,
+};
