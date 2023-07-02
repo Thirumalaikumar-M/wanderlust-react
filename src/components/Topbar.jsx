ec11db912681f1de61451522e3890fe5208f8e7f
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-02 20:39:36 +0530

    Updated assets and Topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,7 @@ import { wanderlustLogo } from '../assets';
 const Topbar = () => {
   return (
     <div className='flex justify-between items-center px-4 py-2'>
-      <img src={wanderlustLogo} alt='wanderlust-logo' className='w-20 h-10 sm:w-40 sm:h-10' />
+      <img src={wanderlustLogo} alt='wanderlust-logo' className='w-[120px] h-[40px] sm:w-[180px] sm:h-[40px]' />
       <div className='flex'>
         <div className='hidden md:flex items-center px-6'>
           <AiOutlineClockCircle size={20} className='mr-2 text-[var(--primary-dark)]' />
