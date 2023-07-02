13e8758be819b62404a2e750d6f97aab3063d257
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-02 13:36:10 +0530

    Add Activities and Booking sections

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { adventure, wildlife, culture, foods, themepark, viewpoint } from '../assets';
 
 const Activities = () => {
   return (
-    <div>Activities</div>
-  )
-}
+    <div id='activities' className='max-w-[1140px] m-auto w-full md:grid grid-cols-3 mt-[-75px]'>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Adventures</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={adventure}
+          alt='adventure'
+        />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Viewpoints</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={viewpoint}
+          alt='viewpoint'
+        />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Cultures</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={culture}
+          alt='culture'
+        />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Foods</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={foods}
+          alt='foods'
+        />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Wildlife</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={wildlife}
+          alt='wildlife'
+        />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Themeparks</h3>
+        <img
+          className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+          src={themepark}
+          alt='themepark'
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Activities
+export default Activities;
--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const Booking = () => {
   return (
-    <div>Booking</div>
-  )
-}
+    <div id='booking' className='max-w-[1140px] m-auto w-full p-4'>
+      <form className='lg:flex lg:justify-between w-full items-center'>
+        <div className='flex flex-col my-2 py-2'>
+          <label>Destinations</label>
+          <select className='lg:w-[300px] md:w-full border rounded-md p-2'>
+            <option>Dubai</option>
+            <option>Maldives</option>
+            <option>Paris</option>
+            <option>Rome</option>
+            <option>India</option>
+          </select>
+        </div>
+        <div className='w-full flex'>
+          <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
+            <label>Check-In</label>
+            <input type='date' />
+          </div>
+          <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
+            <label>Check-Out</label>
+            <input type='date' />
+          </div>
+        </div>
+        <div className='flex flex-col my-2 p-2 w-full'>
+          <label>Search</label>
+          <button className='w-full'>Check Availabilities</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Booking
+export default Booking;
