a30fd5fb08b73a53616c38fdb00b9eb1ddd390be
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-03 13:46:01 +0530

    Add gallery section

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { gallery1, gallery2, gallery3, gallery4, gallery5 } from '../assets';
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <div id='gallery' className='max-w-[1140px] m-auto w-full p-4'>
+      <h2 className='text-center text-gray-700 p-4'>Gallery</h2>
+      <div className='grid sm:grid-cols-5 gap-4'>
+        <div className='sm:col-span-3 col-span-2 row-span-2'>
+          <img className='w-full h-full object-cover' src={gallery1} alt='gallery-img' />
+        </div>
+        <div>
+          <img className='w-full h-full object-cover' src={gallery2} alt='gallery-img' />
+        </div>
+        <div>
+          <img className='w-full h-full object-cover' src={gallery3} alt='gallery-img' />
+        </div>
+        <div>
+          <img className='w-full h-full object-cover' src={gallery4} alt='gallery-img' />
+        </div>
+        <div>
+          <img className='w-full h-full object-cover' src={gallery5} alt='gallery-img' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
