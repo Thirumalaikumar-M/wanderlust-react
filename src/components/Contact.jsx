a2aa65dd2ab29fe64c8d80e20a6dc65b2ecdddfe
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-07-03 16:32:42 +0530

    Add Contact and Footer sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { contactImg } from '../assets';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div id='contact' className='max-w-[1140px] m-auto w-full p-4'>
+      <h2 className='text-center text-gray-700 p-4'>Send us a message</h2>
+      <p className='text-center text-gray-700 py-2'>We're standing by!</p>
+      <div className='grid md:grid-cols-2'>
+        <img className='w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]' src={contactImg} alt='contact-img' />
+        <form>
+          <div className='grid grid-cols-2'>
+            <input className='border m-2 p-2' type='text' placeholder='First name' />
+            <input className='border m-2 p-2' type='text' placeholder='Last name' />
+            <input className='border m-2 p-2' type='email' placeholder='Email' />
+            <input className='border m-2 p-2' type='tel' placeholder='Phone number' />
+            <textarea className='border col-span-2 m-2 p-2' cols={30} rows={10} />
+            <button className='col-span-2 m-2'>Send Message</button>
+          </div>
+        </form> 
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='max-w-[1140px] w-full py-5 m-auto border-t-4'>
+      <p className='text-center text-gray-700'>&copy; {new Date().getFullYear()} Wanderlust. All rights reserved.</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
